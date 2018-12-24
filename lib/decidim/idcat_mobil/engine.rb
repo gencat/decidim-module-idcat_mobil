0782ebd6beccebd1dd609efa8620c0dd2191ef8b
Oliver Valls <oliver.vh@coditramuntana.com>
2018-12-24 11:30:52 +0100

    [FEATURE] Force `Decidim::CurrentOrganization` middleware to be executed before `Warden::Manager` to avoid path generation problems.

--- lib/decidim/idcat_mobil/engine.rb
+++ lib/decidim/idcat_mobil/engine.rb
@@ -15,6 +15,15 @@ module Decidim
         # root to: "idcat_mobil#index"
       end
 
+      #
+      # Force `Decidim::CurrentOrganization` to be executed before `Warden::Manager` to avoid path generation problems.
+      # NOTE: Remove this initializer when in Decidim 0.16.0 where it has been fixed.
+      #
+      initializer "decidim.middleware" do |app|
+        app.config.middleware.delete Decidim::CurrentOrganization
+        app.config.middleware.insert_before Warden::Manager, Decidim::CurrentOrganization
+      end
+
       # initializer "decidim_idcat_mobil.assets" do |app|
       #   app.config.assets.precompile += %w[decidim_idcat_mobil_manifest.js decidim_idcat_mobil_manifest.css]
       # end
