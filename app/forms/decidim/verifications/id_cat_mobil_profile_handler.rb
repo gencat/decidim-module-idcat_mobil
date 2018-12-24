c80a3cbeba6d79fd2a3a1a092e5d57ab68fc57b9
Oliver Valls <oliver.vh@coditramuntana.com>
2018-12-24 10:59:09 +0100

    Work in progress. Shows verifier in user's profile but profile handler does nothing.

---
+++ app/forms/decidim/verifications/id_cat_mobil_profile_handler.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Verifications
+    # A Handler form object that just holds OAuth2 data provided by AOC when performing "IdCat mòbil" authentication.
+    class IdCatMobilProfileHandler < AuthorizationHandler
+      # attribute :oauth_data, Hash
+
+      # validates :unique_id, presence: true
+      # validate :is_idcatmobil_method, :has_ok_status
+
+      def unique_id
+        # oauth_data['identifier']
+      end
+
+      def metadata
+       oauth_data
+      end
+
+      def user
+        # Decidim::User.find(oauth_data[:user_id])
+      end
+
+      #-----------------------------------------------------------
+      private
+      #-----------------------------------------------------------
+
+      def is_idcatmobil_method
+        return true if oauth_data['method'] == 'idcatmobil'
+        errors.add(:base, I18n.t("decidim.verifications.idcat_mobil.errors.invalid_method"))
+        false
+      end
+      def has_ok_status
+        return true if oauth_data['status'] == 'ok'
+        errors.add(:base, I18n.t("decidim.verifications.idcat_mobil.errors.invalid_status"))
+        false
+      end
+    end
+  end
+end
--- lib/decidim/idcat_mobil/engine.rb
+++ lib/decidim/idcat_mobil/engine.rb
@@ -11,8 +11,23 @@ module Decidim
 
       routes do
         # Add engine routes here
-        # resources :idcat_mobil
-        # root to: "idcat_mobil#index"
+        resource :authorizations, only: [:new, :create, :edit, :update], as: :authorization
+
+        root to: "authorizations#new"
+      end
+
+      initializer "idcat_mobil.add_authorization_handlers" do |_app|
+        # Make IdCat mòbil OAuth2 handler accessible in the register:
+        # ```
+        # Decidim::AuthorizationHandler.handler_for(:idcat_mobil, ....)
+        # ```
+        # Decidim::Verifications.register_workflow(:idcat_mobil) do |workflow|
+        #   workflow.form = "Decidim::Verifications::IdCatMobilHandler"
+        # end
+        # Make IdCat mòbil handler in User's profile accessible in the register:
+        Decidim::Verifications.register_workflow(:idcat_mobil_profile) do |workflow|
+          workflow.form = "Decidim::Verifications::IdCatMobilProfileHandler"
+        end
       end
 
       # initializer "decidim_idcat_mobil.assets" do |app|
