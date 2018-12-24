fd1ab5b37811ab90c779856b13742febe9d397d2
Oliver Valls <oliver.vh@coditramuntana.com>
2018-12-24 10:43:41 +0100

    [REFACTOR] Remove useless initializer.

--- config/initializers/decidim_verifications.rb
+++
@@ -1,7 +0,0 @@
-# Make IdCat mòbil handler accessible in the register:
-# ```
-# Decidim::AuthorizationHandler.handler_for(:idcat_mobil, ....)
-# ```
-Decidim::Verifications.register_workflow(:idcat_mobil) do |workflow|
-  workflow.form = "Decidim::Verifications::IdCatMobilHandler"
-end
